3c9b6881a9fa6723c7d414a04dcb8a8e5a7c2db7
unknown <altamashbagwan1@gmail.com>
2022-02-24 18:14:37 +0530

    Refactored: Correct code Hygiene of POST Tweet in twitter through API

--- src/test/java/com/bridgelabz/TwitterAPI.java
+++ src/test/java/com/bridgelabz/TwitterAPI.java
@@ -9,6 +9,7 @@ public class TwitterAPI {
 
     @Test
     public void postTest() {
+
         Response response = RestAssured.given()
                 .auth()
                 .oauth("TTQQO4y0DJGZLBU2LcU1VJECQ",
@@ -23,35 +24,7 @@ public class TwitterAPI {
         String id_str = jsonPath.getString("id_str");
         System.out.println("***************");
         System.out.println(id_str);
-    }
-
-    @Test
-    public void retweetTest() {
-
-        Response response = RestAssured.given()
-                .auth()
-                .oauth("TTQQO4y0DJGZLBU2LcU1VJECQ",
-                        "qkf8Rj3kggsjkvc2mF5sBLvE8k93ZL9Q74nPzKujFwJ8QAofoV",
-                        "4003503614-NqJnAeZ2s6oHIPEWsNJnKDoKB4TtIPD2oOFUSuW",
-                        "CsW3W4jEd4OarUqbqaJSSVzy808HEAB5uyGyZDA9QrLZR")
-                .post("https://api.twitter.com/1.1/statuses/retweet/1496811999564500993.json");
-        System.out.println("status code: " + response.getStatusCode());
-        response.prettyPrint();
 
     }
-
-    @Test
-    public void deleteTest() {
-
-        Response response = RestAssured.given()
-                .auth()
-                .oauth("TTQQO4y0DJGZLBU2LcU1VJECQ",
-                        "qkf8Rj3kggsjkvc2mF5sBLvE8k93ZL9Q74nPzKujFwJ8QAofoV",
-                        "4003503614-NqJnAeZ2s6oHIPEWsNJnKDoKB4TtIPD2oOFUSuW",
-                        "CsW3W4jEd4OarUqbqaJSSVzy808HEAB5uyGyZDA9QrLZR")
-                .delete("https://api.twitter.com/2/tweets/1496811999564500993");
-        System.out.println("status code: " + response.getStatusCode());
-        response.prettyPrint();
-
-    }
+    
 }
